c096365d0de788c9ab6e3d86332918d5bed722a3
Sagar Bhagwatkar <sagarbhagwat1348.sb@gmail.com>
2023-10-22 22:38:50 +0530

    variable day 1 learning

---
+++ Day1/varibles.js
@@ -0,0 +1,38 @@
+// problem 1
+
+console.log("Masai School");
+console.log("A Transformation In Education");
+
+// problem 2 
+
+const name0 = "sagar";
+console.log(name0);
+const fatherName = "Narayan";
+console.log(fatherName);
+const motherName = "shalini"; 
+console.log(motherName);
+
+// problem 3
+
+let name1 = "Sagar";
+console.log(name1);
+console.log(typeof 'name');
+var age = 25;
+console.log(age);
+console.log(typeof 25);
+
+// problem 4
+
+let name2 = "Mukesh Ambani";
+let school = "St. Paul Public School";
+let grade = "A+";
+let section = "B";
+let rollno = 34;
+let subject = "Subjects";
+console.log("Name :"+name2+"     "+"school Name :"+ school);
+console.log("Roll No :"+rollno+"   "+"section :"+section);
+console.log("\n");
+console.log("Score SubjectVise :");
+console.log(subject+"\n"+"math"+                    "      "+"90"+"\n"+"science"+"    "+"98"+"\n"+"chemistry"+"  "+"95"+"\n"+"Result :"+"97%");
+console.log("\n");
+console.log("Grade :"+grade);
